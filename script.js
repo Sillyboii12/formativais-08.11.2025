a782cbe8bf96a98f728cb1537c472a44b1f64827
unknown <bruno@example.com>
2025-11-08 12:58:24 +0200

    working calculator

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// console.log("js loaded");
 const button = document.getElementById("button");
 const input = document.getElementById("input");
 const output = document.getElementById("output");
-// console.log(output, input, button);
 
 button.addEventListener('click', function calc(){
-    console.log("working!")
+    let rad = input.value;
+    let circ = 2 * Math.PI * rad;
+    let area = rad**2 * Math.PI
+
+    output.innerText = `Riņķa līnija ir ${circ.toFixed(2)} cm un riņķa laukums ir ${area.toFixed(2)} cm^2`;
 });
