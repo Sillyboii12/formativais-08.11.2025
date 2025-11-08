468366393fedd3f0427a69b1a15f76b4d8cd012b
unknown <bruno@example.com>
2025-11-08 12:53:19 +0200

    Geting html with ids and seting up button click

---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log("js loaded");
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+// console.log(output, input, button);
+
+button.addEventListener('click', function calc(){
+    console.log("working!")
+});
